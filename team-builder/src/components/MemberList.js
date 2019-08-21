4b289375b7ab37c91846b8c20de8f00d2d287b92
Tyler Gilchrist <tyler668@gmail.com>
2019-08-21 17:12:55 -0400

    Changed the way cards read

--- team-builder/src/components/MemberList.js
+++ team-builder/src/components/MemberList.js
@@ -8,8 +8,8 @@ const MemberList = props => {
         return (
           <div className="memberCard" key={member.id}>
             <h2>{member.name}</h2>
-            <h3>Age: {member.age}</h3>
-            <p>Powers: {member.power}</p>
+            <h3>Age:{member.age}</h3>
+            <p>Powers:{member.power}</p>
           </div>
         );
       })}
